6ec838632a67ac1227c2ea62dd647e422df85738
jomariepolanco <jomariepolanco@gmail.com>
2021-01-18 15:01:39 -0500

    refactored jobs and company serializer

--- app/serializers/company_serializer.rb
+++ app/serializers/company_serializer.rb
@@ -1,5 +1,5 @@
 class CompanySerializer < ActiveModel::Serializer
-  attributes :id, :name, :industry, :address, :poc_name, :poc_email, :poc_number, :users, :calls, :notes
+  attributes :id, :name, :industry, :address, :poc_name, :poc_email, :poc_number, :users, :calls, :notes, :jobs
 
   def users
     self.object.users.uniq
@@ -7,5 +7,21 @@ class CompanySerializer < ActiveModel::Serializer
 
   def calls 
     self.object.client_calls
+  end
+
+  def jobs 
+    return self.object.open_jobs.map do |job|
+      {
+        id: job.id,
+        title: job.title,
+        description: job.description,
+        pay: job.pay,
+        due_date: job.due_date,
+        user: job.user.first_name + ' ' + job.user.last_name,
+        company_id: job.company_id,
+        company: job.company.name,
+        job_applications: job.job_applications
+      }
+    end
   end
 end
--- app/serializers/open_job_serializer.rb
+++ app/serializers/open_job_serializer.rb
@@ -1,5 +1,5 @@
 class OpenJobSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :pay, :due_date, :user_id, :company_id, :poc
+  attributes :id, :title, :description, :pay, :due_date, :user_id, :company_id, :poc, :job_applications
 
   def poc 
     return {
@@ -7,5 +7,19 @@ class OpenJobSerializer < ActiveModel::Serializer
       email: self.object.company.poc_email,
       phone_number: self.object.company.poc_number
     }
+  end
+
+  def job_applications
+    return self.object.job_applications.map do |app|
+      {
+        id: app.id,
+        candidate: app.candidate.first_name + ' ' + app.candidate.last_name,
+        candidate_id: app.candidate_id,
+        open_job_id: app.open_job_id,
+        red: app.red,
+        yellow: app.yellow,
+        green: app.green 
+      }
+    end
   end
 end
