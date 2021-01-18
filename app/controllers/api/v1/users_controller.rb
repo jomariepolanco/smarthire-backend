701e000ec2743ea4ce851acfe5cb4d9fface4634
jomariepolanco <jomariepolanco@gmail.com>
2021-01-18 15:45:47 -0500

    created auth controller and profile method in users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -14,12 +14,16 @@ class Api::V1::UsersController < ApplicationController
     render json: @user
   end
 
+  def profile 
+    render json: {user: UserSerializer.new(current_user)}, status: :accepted
+  end
+
   # POST /users
   def create
     @user = User.new(user_params)
 
     if @user.save
-      @token = encode_token(user_id: @user.id)
+      @token = encode_token({user_id: @user.id})
       render json: {user: UserSerializer.new(@user), jwt: @token}, status: :created
     else
       render json: {error: 'failed to create user'}, status: :not_acceptable
