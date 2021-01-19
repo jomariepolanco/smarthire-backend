0ca4dd6490ea8e46349f4c1817813fd25e043499
jomariepolanco <jomariepolanco@gmail.com>
2021-01-19 07:54:22 -0500

    added name to candidate and user serializer

--- app/serializers/candidate_serializer.rb
+++ app/serializers/candidate_serializer.rb
@@ -1,7 +1,11 @@
 class CandidateSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :cell_phone, :home_phone, :date_of_birth, :address, :city, :state, :zipcode, :calls, :notes 
+  attributes :id, :first_name, :last_name, :name, :cell_phone, :home_phone, :date_of_birth, :address, :city, :state, :zipcode, :calls, :notes 
 
   def calls 
     self.object.candidate_calls 
+  end
+
+  def name 
+    "#{self.object.first_name} #{self.object.last_name}"
   end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email, :tasks, :candidate_calls, :client_calls
+  attributes :id, :first_name, :last_name, :name, :email, :tasks, :candidate_calls, :client_calls
 
   def tasks 
     self.object.projects.each do |pro|
@@ -15,4 +15,7 @@ class UserSerializer < ActiveModel::Serializer
     self.object.client_calls
   end
 
+  def name 
+    "#{self.object.first_name} #{self.object.last_name}"
+  end
 end
